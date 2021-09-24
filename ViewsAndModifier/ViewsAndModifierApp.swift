a279bfb2883025e7ea899891dcb79093b16db924
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-09-24 09:45:58 +0800

    Initial Commit

---
+++ ViewsAndModifier/ViewsAndModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifierApp.swift
+//  ViewsAndModifier
+//
+//  Created by Giovanni Gaffé on 2021/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
